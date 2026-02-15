8331d5ce25b9d4033b408b9703ce56089acbc105
Enes Shehu <104234112+Enes1998@users.noreply.github.com>
2026-02-15 14:14:49 +0100

    Add navigation bar component with Home and Search links

--- frontend/movie-rating-frontend/src/App.jsx
+++ frontend/movie-rating-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import "./App.css";
@@ -10,6 +11,9 @@ function App() {
         <header>
           <h1>Movie Rating App</h1>
         </header>
+        {/* Navbar is rendered on all pages for consistent navigation */}
+        <Navbar />
+
         <Routes>
           <Route path="/" element={<Home />} />
           {/* Dynamic route for movie details page, captures movie ID from URL */}
